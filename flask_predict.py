0070e92d880714533a80c098f5583ecf0fcf702b
Rupali Sinha <rusi02@berlin-212.b.dfki.de>
2020-02-14 15:08:18 +0100

    Changes for Flasgger

--- flask_predict.py
+++ flask_predict.py
@@ -1,5 +1,6 @@
 import pickle
 from flask import Flask, request
+from flasgger import Swagger
 import numpy as np
 import pandas as pd
 
@@ -8,9 +9,30 @@ with open('./model/rf.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 @app.route('/predict')
 def predict_iris():
+    """Example endpoint returning a prediction of iris
+    ---
+    parameters:
+      - name: s_length
+        in: query
+        type: number
+        required: true
+      - name: s_width
+        in: query
+        type: number
+        required: true
+      - name: p_length
+        in: query
+        type: number
+        required: true
+      - name: p_width
+        in: query
+        type: number
+        required: true
+    """
     s_length = request.args.get("s_length")
     s_width = request.args.get("s_width")
     p_length = request.args.get("p_length")
@@ -24,10 +46,19 @@ def predict_iris():
 
 @app.route('/predict_file', methods=["POST"])
 def predict_iris_file():
+    """Example file endpoint returning a prediction of iris
+    ---
+    parameters:
+      - name: input_file
+        in: formData
+        type: file
+        required: true
+    """
+
     input_data = pd.read_csv(request.files.get("input_file"), header=None)
     prediction = model.predict(input_data)
     return str(list(prediction))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
